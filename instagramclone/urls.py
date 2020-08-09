cee3a53a927439c7fffb7512a004cd1a43667f55
Kingsleymuturi <kingsleymuturi9@gmail.com>
2020-08-09 17:27:12 +0300

    add urls file

---
+++ instagramclone/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path, include
+from instagram.views import PostLikeToggle, PostLikeAPIToggle
+from . import views
+
+urlpatterns = [
+    path('signup/', views.signup, name='signup'),
+    path('account/', include('django.contrib.auth.urls')),
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+]
