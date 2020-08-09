bc6383d3a1839c333389e9106b51bbf77adf24ae
Kingsleymuturi <kingsleymuturi9@gmail.com>
2020-08-09 17:12:12 +0300

    add forms on installed apps

--- instagram/settings.py
+++ instagram/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import bootstrap3
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
     'instagramclone',
     'bootstrap3'
 ]
