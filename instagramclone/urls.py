f6466cfb23e42ba5305b86fb1a97332760edb223
Kingsleymuturi <kingsleymuturi9@gmail.com>
2020-08-09 17:28:04 +0300

    add profile path

--- instagramclone/urls.py
+++ instagramclone/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('account/', include('django.contrib.auth.urls')),
     path('', views.index, name='index'),
-    
+    path('profile/<username>/', views.profile, name='profile'),
     
     
     
