9ea24148f6c030051b4be235c5b1d120b69fd828
Kingsleymuturi <kingsleymuturi9@gmail.com>
2020-08-09 17:27:41 +0300

    add home path

--- instagramclone/urls.py
+++ instagramclone/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('account/', include('django.contrib.auth.urls')),
-    
+    path('', views.index, name='index'),
     
     
     
