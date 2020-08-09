a1f6f5be6372866f7d567193f2d17ada47b65d9c
Kingsleymuturi <kingsleymuturi9@gmail.com>
2020-08-09 18:08:54 +0300

    update views

--- instagramclone/views.py
+++ instagramclone/views.py
@@ -46,7 +46,7 @@ def index(request):
         'users': users,
 
     }
-    return render(request, 'instagram/index.html', params)
+    return render(request, 'instagramclone/index.html', params)
 
 
 @login_required(login_url='login')
@@ -68,7 +68,7 @@ def profile(request, username):
         'images': images,
 
     }
-    return render(request, 'instagram/profile.html', params)
+    return render(request, 'instagramclone/profile.html', params)
 
 
 @login_required(login_url='login')
@@ -92,7 +92,7 @@ def user_profile(request, username):
         'follow_status': follow_status
     }
     print(followers)
-    return render(request, 'instagram/user_profile.html', params)
+    return render(request, 'instagramclone/user_profile.html', params)
 
 @login_required(login_url='login')
 def post_comment(request, id):
@@ -116,7 +116,7 @@ def post_comment(request, id):
         'is_liked': is_liked,
         'total_likes': image.total_likes()
     }
-    return render(request, 'instagram/single_post.html', params)
+    return render(request, 'instagramclone/single_post.html', params)
 
 
 class PostLikeToggle(RedirectView):
@@ -177,7 +177,7 @@ def like_post(request):
         'total_likes': image.total_likes()
     }
     if request.is_ajax():
-        html = render_to_string('instagram/like_section.html', params, request=request)
+        html = render_to_string('instagramclone/like_section.html', params, request=request)
         return JsonResponse({'form': html})
 
 
@@ -192,10 +192,10 @@ def search_profile(request):
             'results': results,
             'message': message
         }
-        return render(request, 'instagram/results.html', params)
+        return render(request, 'instagramclone/results.html', params)
     else:
         message = "You haven't searched for any image category"
-    return render(request, 'instagram/results.html', {'message': message})
+    return render(request, 'instagramclone/results.html', {'message': message})
 
 
 def unfollow(request, to_unfollow):
